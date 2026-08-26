daf05a8409814fbce6c774cc820ddf92d954a26a
samscript18 <adeolasam20@gmail.com>
2026-08-26 16:58:43 +0100

    chore: scaffold Next.js application

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const compat = new FlatCompat({ baseDirectory: dirname(fileURLToPath(import.meta.url)) });
+const config = [...compat.extends("next/core-web-vitals", "next/typescript")];
+export default config;
